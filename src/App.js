e206c0d79b7e916911d64caa9bc208049d72e432
rochafialvin <rochafi.dev@gmail.com>
2022-07-19 10:23:19 +0700

    init client

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import socket from "./config/socket";
 
 function App() {
+  const kirimPesan = () => {
+    socket.emit("kirimpesan", { pesan: "hello" });
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>App Component</h1>
+      <button onClick={kirimPesan}>Kirim pesan</button>
     </div>
   );
 }
